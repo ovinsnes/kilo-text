6bb9fa1ffdf20f59b29a6d0feafcc2d0819c3fb4
Ole Vinsnes <Ole.Vinsnes@nhn.no>
2024-10-28 20:49:35 +0100

    turning off control flags

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
-
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
@@ -14,7 +15,8 @@ void enableRawMode(void) {
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_iflag &= ~(ICNRL | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -25,6 +27,12 @@ int main(void) {
 
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
