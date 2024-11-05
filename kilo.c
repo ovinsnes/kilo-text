6ba59b00e4dfdf7d42acf8a6cea898bd7456da45
Ole Vinsnes <Ole.Vinsnes@nhn.no>
2024-11-05 20:51:34 +0100

    arrow keys move cursor

--- kilo.c
+++ kilo.c
@@ -12,7 +12,15 @@
 /*** defines ***/
 
 #define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
 
 /*** data ***/
 
@@ -57,13 +65,33 @@ void enableRawMode(void) {
         die("tcsetattr");
 }
 
-char editorReadKey(void) {
+int editorReadKey(void) {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
 	if (nread == -1 && errno != EAGAIN) die("read");
     }
-    return c;
+
+    // Reading escape sequences as a single keypress
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+        }
+        
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -169,25 +197,25 @@ void editorRefreshScreen(void) {
 
 /*** input ***/
 
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
     switch (key) {
-        case 'h':
+        case ARROW_LEFT:
             E.cx--;
             break;
-        case 'l':
+        case ARROW_RIGHT:
             E.cx++;
             break;
-        case 'k':
+        case ARROW_UP:
             E.cy--;
             break;
-        case 'j':
+        case ARROW_DOWN:
             E.cy++;
             break; 
     }
 }
 
 void editorProcessKeypress(void) {
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):
@@ -197,10 +225,10 @@ void editorProcessKeypress(void) {
             exit(0);
             break;
 
-        case 'h':
-        case 'j':
-        case 'k':
-        case 'l':
+        case ARROW_LEFT:
+        case ARROW_DOWN:
+        case ARROW_UP:
+        case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
     }
